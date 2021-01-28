dd11c1d80662159ee7a3c92bd5f63fd5bdbfed1c
genshen <genshenchu@gmail.com>
2021-01-28 20:59:45 +0800

    build(gui): add a script to build fyne based client-ui using fyne-cross

---
+++ client-ui/fyne-cross-build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# build app using fyne-cross in docker
+
+# How to run: go to root dir of tthis repo, run `./client-ui/fyne-cross-build.sh`
+
+appBuildNumber=2
+appID="wssocks-ustb-client-ui.genshen.github.com"
+appVersion=0.5.1
+appIcon=./client-ui/app-512.png
+buildEnv="GOPROXY=https://goproxy.cn"
+buildFlags="--app-build ${appBuildNumber} --app-id ${appID} --app-version ${appVersion} --icon ${appIcon} --env ${buildEnv}"
+
+fyne-cross linux ${buildFlags} --arch amd64 ./client-ui
+fyne-cross windows ${buildFlags} --arch amd64 ./client-ui
+fyne-cross darwin ${buildFlags} --arch amd64 ./client-ui
