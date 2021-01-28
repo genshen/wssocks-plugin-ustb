422c7a2443daff645699b944532a42ce0274cc14
genshen <genshenchu@gmail.com>
2021-01-28 20:52:32 +0800

    build(swiftui): use -trimpath flag to build `libwssocks_go_api` under extra/go-api

--- extra/go-api/build_archive.sh
+++ extra/go-api/build_archive.sh
@@ -2,6 +2,6 @@
 set -e
 
 # this script build go api into an archive file (static library)
-FLAGS='-gcflags="all=-trimpath=${GOPATH}" -asmflags "all=-trimpath=${GOPATH}"'
+FLAGS='--trimpath'
 
 go build $FLAGS --buildmode=c-archive -o libwssocks_go_api.a
